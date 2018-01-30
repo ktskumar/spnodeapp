bdd5a40551c4002f9d412fe464a24e7e98f803df
Shantha kumar Thamidurai <shanthakumar.t@hcl.com>
2018-01-30 11:06:50 +0530

    Configuration update

--- config/settings.js
+++ config/settings.js
@@ -1,8 +1,8 @@
 var settings = {
             sp: {
-                id: "d3b0117e-7a0c-4a74-9881-f0046094ce6e",
-                secret: "ooY5058FCEumwnWUFojL0nAVY01nrYayf8NIyfr3p1Q=",
-                url: "https://snips.sharepoint.com/sites/dev",             
+                id: "<Client Id>",
+                secret: "<Client Secret>",
+                url: "<SharePoint Site URL>",             
             }        
     }    
 module.exports = settings;
